7f01531499a6130b338405fbf36bc0568ed0bc66
Myugeli <myueyg@gmail.com>
2017-02-03 14:19:52 -0500

    Parallel can run another script now.

--- parallel.py
+++ parallel.py
@@ -1,16 +1,25 @@
 import multiprocessing
 from train import train
+#Change this to actual name
+from main import nameMethod
+
+
 def worker(num,name):
 	print ('Worker: ' + str(num) + ', Game: ' + name) 
-	train()	
+	train()		
+	return
+def nameWorker():
+	print ('nameWorker') 
 	return
 if __name__ == '__main__':
 	jobs = []
 	gameName = 'Breakout'
 	instNum = 3
+	p2 = multiprocessing.Process(target=nameWorker)
+	p2.start()
 	for i in range(instNum):
 		p = multiprocessing.Process(target=worker, args=(i,gameName))
 		jobs.append(p)
 		p.start()
-
+	
 
